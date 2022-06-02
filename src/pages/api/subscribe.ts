cdc8024fbcd7add4c85fff9f6ec91b05df2117d1
Vitor Hugo <job_vitoraraujo@outlook.com>
2022-06-02 09:32:36 -0300

    .
    .

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { stripe } from "../../services/stripe";
+import { getSession } from 'next-auth/react'
+
+export default async function (req: NextApiRequest, res: NextApiResponse){ 
+    if(req.method === 'POST'){
+        const session = await getSession({req})
+
+        const stripeCustomer = await stripe.customers.create({
+            email: session.user.email
+            // metadata
+        })
+
+        const stripeCheckoutSession = await stripe.checkout.sessions.create({
+            customer: stripeCustomer.id,
+            payment_method_types: ['card'],
+            billing_address_collection: 'required',
+            line_items: [{price: 'price_1L0oFdABek0xbiMyYMPSvO53', quantity: 1}], // We have one product, because this we can to put a static Id of the price
+            mode: 'subscription',
+            allow_promotion_codes: true,
+            success_url: process.env.STRIPE_SUCCES_URL,
+            cancel_url: process.env.STRIPE_CANCEL_URL
+        }) 
+        return res.status(200).json({sessionId: stripeCheckoutSession.id})
+    } else {
+        res.setHeader('Allow', 'POST')
+        res.status(405).end('Method not allowed')
+    }
+}
